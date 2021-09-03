3e02900529ed722e9cb3f5cc644407fcccdec6a3
Danny Shoun <danielshoun+servicesgithub@pm.me>
2021-09-03 11:31:09 -0400

    Updated React build script to automatically place in Spring Boot's static folder. Updated Spring WebMvc config to serve the built React app. Fixed an issue with database credentials in application.properties

--- src/main/kotlin/dev/shoun/starter/config/WebMvcConfig.kt
+++ src/main/kotlin/dev/shoun/starter/config/WebMvcConfig.kt
@@ -2,9 +2,17 @@ package dev.shoun.starter.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-@EnableWebMvc
 @Configuration
 class WebMvcConfig: WebMvcConfigurer {
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/")
+            .setViewName("forward:/index.html")
+        registry.addViewController("/{x:[\\w\\-]+}")
+            .setViewName("forward:/index.html")
+        registry.addViewController("/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}")
+            .setViewName("forward:/index.html")
+    }
 }
